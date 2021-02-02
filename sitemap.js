64b2fec478a634518c912210c3f6d276672e57f6
imrodriguez <imanolrodriguezdemaya@gmail.com>
2021-02-02 12:34:49 +0100

    Sitemap

---
+++ sitemap.js
@@ -0,0 +1,18 @@
+const sitemap = require('nextjs-sitemap-generator');  
+
+sitemap({    
+  baseUrl: 'https://mundomexica.com',  
+  ignoredPaths: ['next', 'index'],  
+  pagesDirectory: __dirname + "/.next/server/pages",  
+  targetDirectory : 'public',
+  extraPaths: ['/'],
+  ignoredExtensions: [
+        'png',
+        'jpg',
+        'css',
+        'txt',
+        'js',
+        'json',
+        'webp'
+  ]
+});
