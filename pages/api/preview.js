dfa08d02e679664eeb4e9a37f9e985ac0bd86064
imrodriguez <imanolrodriguezdemaya@gmail.com>
2021-02-10 16:16:40 +0100

    preview

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -22,7 +22,7 @@ export default async function preview(req, res) {
   // Redirect to the path from the fetched post
   // We don't redirect to req.query.slug as that might lead to open redirect vulnerabilities
   // res.writeHead(307, { Location: `/posts/${post.slug}` })
-  const url = `/articulo/${post.fields.url}`
+  const url = `/preview/${post.fields.url}`
 
   res.write(`<!DOCTYPE html><html><head><meta http-equiv="Refresh" content="0; url=${url}" />
   <script>window.location.href = '${url}'</script>
--- pages/preview/[slug].js
+++ pages/preview/[slug].js
@@ -1,5 +1,5 @@
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
-import { getPosts, getPost } from '../../services/Posts';
+import { getPost } from '../../services/Posts';
 import { Container } from '../../components/Container';
 import { ContentBox } from '../../components/ContentBox';
 import { Image } from '../../components/Image';
@@ -24,15 +24,8 @@ export default function Article({ post }) {
     )
 };
 
-export async function getStaticPaths() {
-    const posts = await getPosts();
-    const paths = posts.items.map(item => ({ params: { slug: item.fields.url } }));
-
-    return { paths, fallback: false };
-}
-
-export async function getStaticProps({ params, preview = false }) {
-    const post = await getPost(params.slug, preview);
+export async function getServerSideProps({ params }) {
+    const post = await getPost(params.slug, true);
 
     return { props: { post: post.items[0] } };
 }
