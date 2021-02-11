484d07049a64935abec4ef72f7a64bb8a073ee6e
imrodriguez <imanolrodriguezdemaya@gmail.com>
2021-02-11 12:49:58 +0100

    titles

--- components/Content/styles.js
+++ components/Content/styles.js
@@ -18,4 +18,41 @@ export const Wrapper = styled.div`
     p {
         min-height: 1.5em;
     }
+
+    h1, h2, h3 {
+        font-family: ${props => props.theme.fonts.primary};
+    }
+
+    h1 {
+        font-size: 3em;
+        margin-bottom: 0;
+        text-align: center;
+
+        @media only screen and (max-width: ${props => props.theme.breakpoints.mobile}px) {
+            font-size: 2em;
+        }
+    }
+
+    h2 {
+        font-size: 2.5em;
+        text-align: center;
+    }
+
+    h3 {
+        font-size: 2em;
+        text-align: center;
+    }
+
+    p {
+        font-family: ${props => props.theme.fonts.secondary};
+        text-align: justify;
+        font-size: 1.3em;
+    }
+
+    figcaption {
+        background: #eee;
+        text-align: center;
+        padding: .7em;
+        margin-top: -.3em;
+    }
 `;
