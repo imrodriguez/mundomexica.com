a4b215453b3b6e0a81d8e733f8ea0b8ec08d5177
imrodriguez <imanolrodriguezdemaya@gmail.com>
2021-02-19 19:44:41 +0100

    analitycs

--- pages/_document.js
+++ pages/_document.js
@@ -41,19 +41,15 @@ export default class MyDocument extends Document {
           <meta name="theme-color" content="#ffffff" />
           {this.props.styleTags}
 
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=G-0QRWQ5EG14`}
-          />
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-0QRWQ5EG14"></script>
           <script
             dangerouslySetInnerHTML={{
               __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-0QRWQ5EG14', {
-              page_path: window.location.pathname,
-            });
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            
+              gtag('config', 'G-0QRWQ5EG14');
           `,
             }}
           />
