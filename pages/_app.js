5cd406f376e457c7a347a98cc1c72ab869e9c307
imrodriguez <imanolrodriguezdemaya@gmail.com>
2021-02-19 19:40:59 +0100

    analitycs

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,10 @@
 import { config } from '@fortawesome/fontawesome-svg-core';
 import { Layout } from '../components/Layout';
-import { useEffect } from 'react';
 import '@fortawesome/fontawesome-svg-core/styles.css';
-import ReactGA from 'react-ga';
 
 config.autoAddCss = false;
 
 export default function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    ReactGA.initialize('G-0QRWQ5EG14');
-    ReactGA.pageview(window.location.pathname + window.location.search);
-  }, []);
-
   return (
     <Layout>
       <Component {...pageProps} />
--- pages/_document.js
+++ pages/_document.js
@@ -40,6 +40,23 @@ export default class MyDocument extends Document {
           <meta name="msapplication-TileColor" content="#da532c" />
           <meta name="theme-color" content="#ffffff" />
           {this.props.styleTags}
+
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=G-0QRWQ5EG14`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-0QRWQ5EG14', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
         </Head>
         <body>
           <Main />
