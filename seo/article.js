74990405419b90508e048a479a7502809e48b5d1
imrodriguez <irodriguez@leadtech.com>
2021-02-24 17:27:52 +0100

    opengraph

--- seo/article.js
+++ seo/article.js
@@ -1,48 +1,50 @@
-import generalData from '../config/generalData.json';
-import { NextSeo, ArticleJsonLd } from 'next-seo';
+import generalData from "../config/generalData.json";
+import { NextSeo, ArticleJsonLd } from "next-seo";
 
 const ArticleSeo = ({ article }) => (
-    <>
-        <ArticleJsonLd
-            url={`https://mundomexica.com/articulo/${article.fields.url}`}
-            title={article.fields.title}
-            images={[
-                `https:${article.fields.image.fields.file.url}`
-            ]}
-            datePublished={article.sys.createdAt}
-            dateModified={article.sys.updatedAt}
-            authorName={['Ariadna Rodriguez Marquez', 'Mundo Mexica']}
-            publisherName="Ariadna Rodriguez Marquez"
-            publisherLogo={`https:${generalData.logo.fields.file.url}`}
-            description={article.fields.metaDescription}
-        />
-        <NextSeo
-            title={`${article.fields.title} | ${generalData.name}`}
-            description={article.fields.metaDescription}
-            canonical={`https://mundomexica.com/articulo/${article.fields.url}`}
-            additionalMetaTags={[{
-                name: 'keywords',
-                content: article.fields.keywords
-            }]}
-            openGraph={{
-                url: `https://mundomexica.com/articulo/${article.fields.url}`,
-                title: article.fields.title,
-                description: article.fields.metaDescription,
-                images: [
-                    {
-                        url: `https:${article.fields.image.fields.file.url}`,
-                        alt: article.fields.image.fields.title,
-                    },
-                ],
-                site_name: generalData.name,
-            }}
-            twitter={{
-                handle: '@handle',
-                site: '@site',
-                cardType: 'summary_large_image',
-            }}
-        />
-    </>
+  <>
+    <ArticleJsonLd
+      url={`https://mundomexica.com/articulo/${article.fields.url}`}
+      title={article.fields.title}
+      images={[`https:${article.fields.image.fields.file.url}`]}
+      datePublished={article.sys.createdAt}
+      dateModified={article.sys.updatedAt}
+      authorName={["Ariadna Rodriguez Marquez", "Mundo Mexica"]}
+      publisherName="Ariadna Rodriguez Marquez"
+      publisherLogo={`https:${generalData.logo.fields.file.url}`}
+      description={article.fields.metaDescription}
+    />
+    <NextSeo
+      title={`${article.fields.title} | ${generalData.name}`}
+      description={article.fields.metaDescription}
+      canonical={`https://mundomexica.com/articulo/${article.fields.url}`}
+      additionalMetaTags={[
+        {
+          name: "keywords",
+          content: article.fields.keywords,
+        },
+      ]}
+      openGraph={{
+        url: `https://mundomexica.com/articulo/${article.fields.url}`,
+        title: article.fields.title,
+        description: article.fields.metaDescription,
+        images: [
+          {
+            url: `https:${article.fields.image.fields.file.url}`,
+            alt: article.fields.image.fields.title,
+          },
+        ],
+        site_name: generalData.name,
+        type: "website",
+        locale: "es_ES",
+      }}
+      twitter={{
+        handle: "@handle",
+        site: "@site",
+        cardType: "summary_large_image",
+      }}
+    />
+  </>
 );
 
 export { ArticleSeo };
--- seo/category.js
+++ seo/category.js
@@ -1,31 +1,33 @@
-import generalData from '../config/generalData.json';
-import { NextSeo } from 'next-seo';
+import generalData from "../config/generalData.json";
+import { NextSeo } from "next-seo";
 
 const CategorySeo = ({ category }) => (
-    <>
-        <NextSeo
-            title={`${category.fields.name} | ${generalData.name}`}
-            description={category.fields.metaDescription}
-            canonical={`https://mundomexica.com/categoria/${category.fields.url}`}
-            openGraph={{
-                url: `https://mundomexica.com/categoria/${category.fields.url}`,
-                title: category.fields.name,
-                description: category.fields.metaDescription,
-                site_name: generalData.name,
-                images: [
-                    {
-                        url: `https:${generalData.defautOpengraph.fields.file.url}`,
-                        alt: generalData.defautOpengraph.fields.title,
-                    },
-                ],
-            }}
-            twitter={{
-                handle: '@handle',
-                site: '@site',
-                cardType: 'summary_large_image',
-            }}
-        />
-    </>
+  <>
+    <NextSeo
+      title={`${category.fields.name} | ${generalData.name}`}
+      description={category.fields.metaDescription}
+      canonical={`https://mundomexica.com/categoria/${category.fields.url}`}
+      openGraph={{
+        url: `https://mundomexica.com/categoria/${category.fields.url}`,
+        title: category.fields.name,
+        description: category.fields.metaDescription,
+        site_name: generalData.name,
+        images: [
+          {
+            url: `https:${generalData.defautOpengraph.fields.file.url}`,
+            alt: generalData.defautOpengraph.fields.title,
+          },
+        ],
+        type: "website",
+        locale: "es_ES",
+      }}
+      twitter={{
+        handle: "@handle",
+        site: "@site",
+        cardType: "summary_large_image",
+      }}
+    />
+  </>
 );
 
 export { CategorySeo };
