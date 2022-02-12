02904b3c8fcb2f5df0ac591a5d27d6bb43a1130d
imanol Rodríguez de maya <imanolrodriguezdemaya@imanols-MacBook-Pro-2.local>
2022-02-12 17:13:52 +0100

    metricool tracker

--- pages/_document.js
+++ pages/_document.js
@@ -62,6 +62,10 @@ export default class MyDocument extends Document {
           `,
             }}
           />
+          <script dangerouslySetInnerHTML={{ __html: `
+          function loadScript(a){var b=document.getElementsByTagName("head")[0],c=document.createElement("script");c.type="text/javascript",c.src="https://tracker.metricool.com/resources/be.js",c.onreadystatechange=a,c.onload=a,b.appendChild(c)}loadScript(function(){beTracker.t({hash:"ed693dc20b27e90074954a3dc5d909cf"})});
+          `}} 
+          />
         </HeadCSP>
         <body>
           <Main />
